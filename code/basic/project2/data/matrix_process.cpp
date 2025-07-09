ac7576613f3b3b3bcd72a84bdf4f31a306b2a869
Wings236 <932629613@qq.com>
2025-07-10 00:42:16 +0800

    init data process

--- code/basic/project2/data/matrix_process.cpp
+++ code/basic/project2/data/matrix_process.cpp
@@ -1,9 +1,26 @@
 # include <math.h>
 # include <iostream>
+# include <fstream>
 
 using namespace std;
 
 int main(){
+    // 打开文件
+    ofstream outFile("output.txt");
+    ifstream inFile("123.txt");
+
+    if (!outFile){
+        cerr << "文件打开失败" << endl;
+        return 1;
+    }
+
+    outFile << "第一行内容\n";
+    outFile << 42 << " " << 3.14;
+    outFile.close();
+
+    // 追加模式写入（保留原内容）
+    ofstream appendFile("output.txt", ios::app);
+    appendFile << "\n追加的内容";
 
     return 0;
 }

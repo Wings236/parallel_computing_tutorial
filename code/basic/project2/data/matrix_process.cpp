30cd38c46d26feabcbf02a2c104b28af636cc17a
Wings236 <932629613@qq.com>
2025-07-10 11:00:31 +0800

    data process

--- code/basic/project2/data/matrix_process.cpp
+++ code/basic/project2/data/matrix_process.cpp
@@ -1,26 +1,67 @@
-# include <math.h>
-# include <iostream>
-# include <fstream>
+#include <random>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <chrono>
 
 using namespace std;
 
 int main(){
-    // 打开文件
-    ofstream outFile("output.txt");
-    ifstream inFile("123.txt");
+    int matSize[] = {32, 256, 2048};
 
-    if (!outFile){
-        cerr << "文件打开失败" << endl;
-        return 1;
+    for (int i = 0; i < 3; i++){
+        cout << matSize[i] << ":";
+        // 构建矩阵数据  NxN * NxN -> NxN
+        double * matA = new double[(matSize[i]*matSize[i]) * sizeof(int)];
+        double * matB = new double[(matSize[i]*matSize[i]) * sizeof(int)];
+        double * matRes = new double[(matSize[i]*matSize[i]) * sizeof(int)];
+
+        auto start = chrono::high_resolution_clock::now();
+        for (int j = 0; j < matSize[i]; j++){
+            for (int k = 0; k < matSize[i]; k++){
+                matA[j*matSize[i] + k] = double(rand())/RAND_MAX;
+                matB[j*matSize[i] + k] = double(rand())/RAND_MAX;
+            }
+        }
+
+        // 计算结果
+        for (int j = 0; j< matSize[i]; j++){
+            for(int k = 0; k < matSize[i]; k++){
+                double temp = 0;
+                for (int l = 0; l < matSize[i]; l++){
+                    temp += matA[j*matSize[i] + l] * matA[l*matSize[i] + j];
+                }
+                matRes[j*matSize[i] + k] = temp;
+            }
+        }
+        // 输出文件
+        ofstream matAFile("mat-A-" + to_string(matSize[i]) + ".txt");
+        ofstream matBFile("mat-B-" + to_string(matSize[i]) + ".txt");
+        ofstream matResFile("mat-Res-" + to_string(matSize[i]) + ".txt");
+
+        for (int j = 0; j < matSize[i]; j ++){
+            for (int k = 0; k < matSize[i];k++){
+                matAFile << fixed << setprecision(6) << matA[j*matSize[i] + k] << " ";
+                matBFile << fixed << setprecision(6) << matB[j*matSize[i] + k] << " ";
+                matResFile << fixed << setprecision(6) << matRes[j*matSize[i] + k] << " ";
+            }
+            matAFile << endl;
+            matBFile << endl;
+            matResFile << endl;
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        chrono::duration<double> duration = end - start;
+        std::cout << "Time taken: " << fixed << setprecision(10) << duration.count() << " seconds\n";
+
+        // 关闭
+        matAFile.close();
+        matBFile.close();
+        matResFile.close();
+
+        delete [] matA;
+        delete [] matB;
+        delete [] matRes;
     }
-
-    outFile << "第一行内容\n";
-    outFile << 42 << " " << 3.14;
-    outFile.close();
-
-    // 追加模式写入（保留原内容）
-    ofstream appendFile("output.txt", ios::app);
-    appendFile << "\n追加的内容";
 
     return 0;
 }

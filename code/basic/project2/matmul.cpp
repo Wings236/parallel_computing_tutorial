254f28a8a75bf4b80fcb193748ca6c24034f4690
Wings236 <932629613@qq.com>
2025-07-10 11:06:38 +0800

    project 2 init

--- code/basic/project2/matmul.cpp
+++ code/basic/project2/matmul.cpp
@@ -1,1 +1,20 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    int matSize[] ={32, 256, 2048};
+    for (int i = 0; i < 3; i++){
+        cout << matSize[i] << endl;
+        ifstream matAFile("mat-A-" + to_string(matSize[i]) + ".txt");
+        ifstream matBFile("mat-B-" + to_string(matSize[i]) + ".txt");
+        ifstream matResFile("mat-Res-" + to_string(matSize[i]) + ".txt");
+
+        matAFile.close();
+        matBFile.close();
+        matResFile.close();
+
+    }
+    return 0;
+}

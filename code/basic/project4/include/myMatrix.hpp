75e96292fb52308618d4d2b5873aa20faf7c6f88
Wings236 <932629613@qq.com>
2025-07-29 11:41:38 +0800

    proj4 update: impl function and test

--- code/basic/project4/include/myMatrix.hpp
+++ code/basic/project4/include/myMatrix.hpp
@@ -36,19 +36,122 @@ private:
     }
 
 public:
-    myMatrix(size_t rows, size_t cols, size_t channels=1);
-    myMatrix(size_t rows, size_t cols, size_t channels, const T* dataptr);
-    myMatrix(const myMatrix &A);
-    ~myMatrix();
+    myMatrix():rows(0), cols(0), channels(0), pdata(nullptr), refcount(nullptr){
+        init(0, 0, 0);
+    }
+
+
+    myMatrix(size_t rows, size_t cols, size_t channels=1):rows(rows), cols(cols), channels(channels), pdata(nullptr), refcount(nullptr){
+        init(rows, cols, channels);
+    }
+
+
+    myMatrix(size_t rows, size_t cols, size_t channels, const T* dataptr):rows(rows), cols(cols), channels(channels), pdata(nullptr), refcount(nullptr){
+        init(rows, cols, channels, dataptr);
+    }
+
+
+    myMatrix(const myMatrix &A):rows(A.rows), cols(A.cols), channels(A.channels), pdata(A.pdata), refcount(A.refcount){
+        (*refcount)++;
+    }
+
+
+    ~myMatrix(){
+        release();
+    }
 
     // operator
-    myMatrix& operator=(const myMatrix& A);
-    bool operator==(const myMatrix& A);
-    myMatrix& operator+(const myMatrix& A);
-    myMatrix& operator-(const myMatrix& A);
-    myMatrix& operator*(const myMatrix& A);
+    myMatrix& operator=(const myMatrix& A){
+        this->rows = A.rows;
+        this->cols = A.cols;
+        this->channels = A.channels;
+        this->pdata = A.pdata;
+        this->refcount = A.refcount;
+        *(this->refcount)++;
+        return *(this);
+    }
+
+
+    bool operator==(const myMatrix& A){
+        return (this->pdata == A.pdata);
+    }
+
+
+    myMatrix& operator+(const myMatrix& A){
+        if (this->rows == A.rows && this->cols == A.cols && this->channels == A.channels){
+            myMatrix *temp = new myMatrix(A.rows, A.cols, A.channels);
+            for(int i = 0; i < this->channels; i++){
+                for(int j = 0; j < this->rows; j++){
+                    for(int k = 0; k < this->cols; k++){
+                        temp->pdata[i*(this->rows*this->cols) + j *(this->cols) + k] = this->pdata[i*(this->rows*this->cols) + j *(this->cols) + k] + A.pdata[i*(this->rows*this->cols) + j *(this->cols) + k];
+                    }
+                }
+            }
+            return *(temp);
+        }
+        else {
+            // 什么也不做，直接返回原来的矩阵
+            return *(new myMatrix(0, 0, 0));
+        }
+    }
+
+
+    myMatrix& operator-(const myMatrix& A){
+        if (this->rows == A.rows && this->cols == A.cols && this->channels == A.channels){
+            myMatrix *temp = new myMatrix(A.rows, A.cols, A.channels);
+            for(int i = 0; i < this->channels; i++){
+                for(int j = 0; j < this->rows; j++){
+                    for(int k = 0; k < this->cols; k++){
+                        temp->pdata[i*(this->rows*this->cols) + j *(this->cols) + k] = this->pdata[i*(this->rows*this->cols) + j *(this->cols) + k] - A.pdata[i*(this->rows*this->cols) + j *(this->cols) + k];
+                    }
+                }
+            }
+            return *(temp);
+        }
+        else {
+            // 什么也不做，直接返回原来的矩阵
+            return *(new myMatrix(0, 0, 0));
+        }
+    }
+
+
+    myMatrix& operator*(const myMatrix& A){
+        // 计算方式，同一通道的进行矩阵相乘 (c1, r1, l1) * (c2, r2, l2) -> c1 == c2 && l1 == r2
+        if (this->channels == A.channels && this->cols == A.rows){
+            myMatrix *temp = new myMatrix(this->rows, A.cols, A.channels);
+            for(int i = 0; i < this->channels; i++){
+                // 每一个通道进行矩阵相乘
+                for(int j = 0; j < this->rows; j++){
+                    for(int k = 0; k < A.cols; k++){
+                        T temp_value = 0;
+                        for(int l = 0; l < this->cols; l++){
+                            temp_value += this->pdata[i*(this->rows*this->cols) + j *(this->cols) + l] * A.pdata[i*(A.rows*A.cols) + l*(A.cols) + k];
+                        }
+                        temp->pdata[i*(temp->rows*temp->cols) + j *(temp->cols) + k] = temp_value;
+                    }
+                }
+            }
+            return *(temp);
+        }
+        else {
+            // 什么也不做，直接返回原来的矩阵
+            return *(new myMatrix(0, 0, 0));
+        }
+    }
 
     // others
-    void display();
+    void display(){
+        for(size_t i = 0; i < channels; i++){
+            std::cout << "channel:" << i << std::endl;
+            for(size_t j = 0; j < rows; j++){
+                for(size_t k = 0; k < cols; k++){
+                    std::cout<< this->pdata[i*(this->rows*this->cols) + j*(this->cols) + k] << " ";
+                }
+                std::cout << "\n";
+            }
+        }
+    }
+
 };
+
 #endif
--- code/basic/project4/src/main.cpp
+++ code/basic/project4/src/main.cpp
@@ -3,6 +3,26 @@
 using namespace std;
 
 int main(){
-    myMatrix<float> A(1, 1, 1);
+    float A_value[] = {.1f, .2f, .3f, .4f, .5f, .6f, .7f, .8f};
+    float B_value[] = {.2f, .3f, .4f, .5f, .6f, .7f, .8f, 0.9f};
+    myMatrix<float> A(2, 2, 2, A_value);
+    myMatrix<float> B(2, 2, 2, B_value);
+    cout << "A:" << endl;
+    A.display();
+    cout << "B:" << endl;
+    B.display();
+
+    myMatrix<float> C;
+    C = A + B;
+    cout << "C=A+B:" << endl;
+    C.display();
+
+    myMatrix<float> D = B - A;
+    cout << "D=B-A:" << endl;
+    D.display();
+
+    myMatrix<float> E = A * B;
+    cout << "E=A*B:" << endl;
+    E.display();
     return 0;
 }

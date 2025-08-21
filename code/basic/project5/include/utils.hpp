f9e3c86cadb085f71ab9cdb2e22268af789896ab
Wings236 <932629613@qq.com>
2025-08-21 10:56:28 +0800

    basic proj5 update: img read

---
+++ code/basic/project5/include/utils.hpp
@@ -0,0 +1,16 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+void image2input();
+
+
+void conv2d();
+
+
+void denseMatrixMultiple();
+
+
+void choose();
+
+
+#endif
--- code/basic/project5/src/main.cpp
+++ code/basic/project5/src/main.cpp
@@ -1,9 +1,55 @@
+#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <fstream>
+#include <string>
 
-using namespace std;
+using namespace cv;
 
-int main()
+
+int main(int argc, char* argv[])
 {
-    cout << "this is a simple CPP CNN" << endl;
+    // image path
+    if(argc == 2)
+    {
+        std::string img_path = argv[1];
+        std::cout << img_path << std::endl;
+        // check the path valid
+        Mat img = imread(img_path, IMREAD_COLOR);
+        float img_value_R[img.rows*img.cols];
+        float img_value_G[img.rows*img.cols];
+        float img_value_B[img.rows*img.cols];
+
+        // test
+        for(int i = 0; i < img.rows; i++)
+        {
+            for(int j = 0; j < img.cols; j++)
+            {
+                img_value_R[i*img.size[1] + j] = img.data[i*img.step[0] + j*img.step[1] + 2] / 255.0f;
+                img_value_G[i*img.size[1] + j] = img.data[i*img.step[0] + j*img.step[1] + 1] / 255.0f;
+                img_value_B[i*img.size[1] + j] = img.data[i*img.step[0] + j*img.step[1]] / 255.0f;
+            }
+        }
+        std::cout <<"R[0]: " << img_value_R[128*128-8000] << std::endl;
+        std::cout <<"G[0]: " << img_value_G[128*128-8000] << std::endl;
+        std::cout <<"B[0]: " << img_value_B[128*128-8000] << std::endl;
+
+        return 0;
+    }
+    else
+    {
+        std::cerr << "must input a avlid image path." << std::endl;
+    }
+
+    // calculate
+
+
+
+
+
+
+
+
+
+    std::cout << "this is an simple CPP CNN" << std::endl;
     return 0;
 }

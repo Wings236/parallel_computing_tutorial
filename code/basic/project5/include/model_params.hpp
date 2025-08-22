9fec039e2498abbc13cacac68664dc6f8c35b260
Wings236 <932629613@qq.com>
2025-08-22 11:57:05 +0800

    basic proj5 update: model param and cal

---
+++ code/basic/project5/include/model_params.hpp
@@ -0,0 +1,35 @@
+#ifndef MODEL_PARAMS_H
+#define MODEL_PARAMS_H
+
+typedef struct conv_param {
+    int pad;
+    int stride;
+    int kernel_size;
+    int in_channels;
+    int out_channels;
+    float* p_weight;
+    float* p_bias;
+} conv_param;
+
+
+typedef struct max_pool
+{
+    int pad;
+    int stride;
+    int kernel_size;
+} max_pool;
+
+
+typedef struct fc_param {
+    int in_features;
+    int out_features;
+    float* p_weight;
+    float* p_bias;
+} fc_param;
+
+
+extern conv_param conv_params[3];
+extern max_pool max_pools[2];
+extern fc_param fc_params[1];
+
+#endif
--- code/basic/project5/src/main.cpp
+++ code/basic/project5/src/main.cpp
@@ -1,13 +1,14 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
-#include <fstream>
-#include <string>
+#include "model_params.hpp"
 
 using namespace cv;
 
 
 int main(int argc, char* argv[])
 {
+    float* img_value = nullptr;
+    int intput_channels, intput_rows, intput_cols;
     // image path
     if(argc == 2)
     {
@@ -15,41 +16,58 @@ int main(int argc, char* argv[])
         std::cout << img_path << std::endl;
         // check the path valid
         Mat img = imread(img_path, IMREAD_COLOR);
-        float img_value_R[img.rows*img.cols];
-        float img_value_G[img.rows*img.cols];
-        float img_value_B[img.rows*img.cols];
+        intput_channels = img.channels();
+        intput_rows = img.rows;
+        intput_cols = img.cols;
+        float* value = new float[intput_channels * intput_rows * intput_cols];
 
-        // test
+        // [0, 255] -> [0, 1]
+        int matSize = img.rows * img.cols;
         for(int i = 0; i < img.rows; i++)
         {
             for(int j = 0; j < img.cols; j++)
             {
-                img_value_R[i*img.size[1] + j] = img.data[i*img.step[0] + j*img.step[1] + 2] / 255.0f;
-                img_value_G[i*img.size[1] + j] = img.data[i*img.step[0] + j*img.step[1] + 1] / 255.0f;
-                img_value_B[i*img.size[1] + j] = img.data[i*img.step[0] + j*img.step[1]] / 255.0f;
+                // R G B
+                value[0*matSize + i*img.size[1] + j] = img.data[i*img.step[0] + j*img.step[1] + 2] / 255.0f;
+                value[1*matSize + i*img.size[1] + j] = img.data[i*img.step[0] + j*img.step[1] + 1] / 255.0f;
+                value[2*matSize + i*img.size[1] + j] = img.data[i*img.step[0] + j*img.step[1] + 0] / 255.0f;
             }
         }
-        std::cout <<"R[0]: " << img_value_R[128*128-8000] << std::endl;
-        std::cout <<"G[0]: " << img_value_G[128*128-8000] << std::endl;
-        std::cout <<"B[0]: " << img_value_B[128*128-8000] << std::endl;
-
-        return 0;
+        img_value = value;
     }
     else
     {
         std::cerr << "must input a avlid image path." << std::endl;
+        return -1;
     }
-
     // calculate
 
+    // Layer 1 CNN 3*128*128 -> 16*64*64
+    int Layer_1_channels = conv_params[0].out_channels;
+    int Layer_1_rows = (intput_rows + conv_params[0].pad*2)/(conv_params[0].stride)-(conv_params[0].kernel_size-2);
+    int Layer_1_cols = (intput_cols + conv_params[0].pad*2)/(conv_params[0].stride)-(conv_params[0].kernel_size-2);
+
+    std::cout << Layer_1_channels << std::endl;
+    std::cout << Layer_1_rows << std::endl;
+    std::cout << Layer_1_cols << std::endl;
+
+    float* Layer_1_output = new float[Layer_1_channels*Layer_1_rows*Layer_1_cols];
+
+    for(int i = 0; i < 10; i++){
+        std::cout << "i: "<< i << " value: "<< conv_params[0].p_weight[i] << std::endl;
+    }
+
+    // MaxPool 16*64*64 -> 16*32*32
 
 
 
 
 
 
-
-
+    // free
     std::cout << "this is an simple CPP CNN" << std::endl;
+    delete [] img_value;
+    delete [] Layer_1_output;
+    img_value = nullptr;
     return 0;
 }
